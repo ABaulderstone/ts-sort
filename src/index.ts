df50dc4fd9a922ac2c4d13130c19da18e0198a45
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-08-27 17:46:09 +1000

    linked list implementation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const collection = new NumbersCollection([10, 3, -5, 0]);
 
-const collection = new CharactersCollection('XazYbLs');
+// const collection = new CharactersCollection('XazYbLs');
 
-console.log(collection);
+// console.log(collection);
 
-const sorter = new Sorter(collection);
+// const sorter = new Sorter(collection);
+// sorter.sort();
+// console.log(collection.data);
+
+const list = new LinkedList();
+list.add(500);
+list.add(-10);
+list.add(-3);
+list.add(4);
+
+const sorter = new Sorter(list);
 sorter.sort();
-console.log(collection.data);
+list.print();

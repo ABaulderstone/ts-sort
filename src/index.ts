252055c85e905ba132d7295f0717c11e4297049f
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-08-28 23:15:15 +1000

    finished example

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,22 @@ import { Sorter } from './Sorter';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const collection = new NumbersCollection([10, 3, -5, 0]);
+const collection = new NumbersCollection([10, 3, -5, 0]);
+collection.sort();
+console.log(collection.data);
 
-// const collection = new CharactersCollection('XazYbLs');
-
-// console.log(collection);
-
-// const sorter = new Sorter(collection);
-// sorter.sort();
-// console.log(collection.data);
+const charCollection = new CharactersCollection('Xaaybxakjls');
+charCollection.sort();
+console.log(charCollection.data);
 
 const list = new LinkedList();
 list.add(500);
 list.add(-10);
 list.add(-3);
 list.add(4);
-
-const sorter = new Sorter(list);
-sorter.sort();
+list.sort();
 list.print();
+
+// const sorter = new Sorter(list);
+// sorter.sort();
+// list.print();
